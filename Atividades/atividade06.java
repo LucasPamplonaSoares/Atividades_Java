3af64e617a4a1ba0267cba1794a4f68a4efe558f
Lucas <lucasreitz25@gmail.com>
2022-04-29 14:02:43 -0300

    master

---
+++ Atividades/atividade06.java
@@ -0,0 +1,9 @@
+// Escreva um programa para determinar a quantidade de cavalos necessários para se levantar uma massa de m quilogramas a uma altura de h metros em t segundos. Considere cavalos = (m * h / t) / 745,6999
+
+package Atividades;
+
+public class atividade06 {
+    public static void main(String[] args) {
+        
+    }
+}
